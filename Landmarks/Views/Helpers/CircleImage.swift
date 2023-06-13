7e02bfdc7bfb86a255a541065f96bb9d0023ff2f
manorajesh <mano@raje.sh>
2023-06-13 15:54:59 -0700

    Finished first 3 exercises

---
+++ Landmarks/Views/Helpers/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Mano Rajesh on 6/13/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.gray, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
